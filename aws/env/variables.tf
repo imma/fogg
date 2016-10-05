8a6ea80e8ef4d1287dee694241d6d00f5ddb22b7
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-05 00:27:28 -0700

    output public/private zone

--- aws/env/variables.tf
+++ aws/env/variables.tf
@@ -16,6 +16,30 @@ output "vpc_id" {
   value = "${aws_vpc.env.id}"
 }
 
-output "domain_name" {
-  value = "${data.terraform_remote_state.global.domain_name}"
+output "igw_id" {
+  value = "${aws_internet_gateway.env.id}"
+}
+
+output "public_zone_id" {
+  value = "${aws_route53_zone.public.zone_id}"
+}
+
+output "public_zone_servers" {
+  value = "${aws_route53_zone.public.name_servers}"
+}
+
+output "public_zone_name" {
+  value = "${lookup(map("1",var.env_zone,"0",var.env_name),format("%d",signum(length(var.env_zone))))}.${lookup(map("1",var.env_domain_name,"0",data.terraform_remote_state.global.domain_name),format("%d",signum(length(var.env_domain_name))))}"
+}
+
+output "private_zone_id" {
+  value = "${aws_route53_zone.private.zone_id}"
+}
+
+output "private_zone_servers" {
+  value = "${aws_route53_zone.private.name_servers}"
+}
+
+output "private_zone_name" {
+  value = "${lookup(map("1",var.env_zone,"0",var.env_name),format("%d",signum(length(var.env_zone))))}.${lookup(map("1",var.env_domain_name,"0",data.terraform_remote_state.global.domain_name),format("%d",signum(length(var.env_domain_name))))}"
 }
