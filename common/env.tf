3d10c3f56bb82d1745638396f5218e7c54c2bc49
Defn Nghiem <iam@defn.sh>
2017-03-19 04:50:34 -0700

    use same region, bucket, and derive path from the backend state

--- common/env.tf
+++ common/env.tf
@@ -2,9 +2,9 @@
 module "env" {
   source = "../../../module/env"
 
-  global_bucket = "${var.global_bucket}"
-  global_key    = "${var.global_key}"
-  global_region = "${var.global_region}"
+  global_bucket = "${var.remote_bucket}"
+  global_key    = "${slice(split("-",var.remote_path),0,1)}/terraform.tfstate"
+  global_region = "${var.remote_region}"
 
   az_count = "${var.az_count}"
   env_name = "${var.env_name}"
