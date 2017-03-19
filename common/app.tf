4c9f2c7ad52620a9192407612139321c04ed48c5
Defn Nghiem <iam@defn.sh>
2017-03-19 05:08:30 -0700

    generate more app stubs

--- common/app.tf
+++ common/app.tf
@@ -1,13 +1,16 @@
-# module
+variable "remote_bucket" {}
+variable "remote_path" {}
+variable "remote_region" {}
+
 module "app" {
   source = "../../../../module/app"
 
   global_bucket = "${var.remote_bucket}"
-  global_key    = "${slice(split("_",var.remote_path),0,1)}/terraform.tfstate"
+  global_key    = "${join("_",slice(split("_",var.remote_path),0,1))}/terraform.tfstate"
   global_region = "${var.remote_region}"
 
   env_bucket = "${var.remote_bucket}"
-  env_key    = "${slice(split("_",var.remote_path),0,2)}/terraform.tfstate"
+  env_key    = "${join("_",slice(split("_",var.remote_path),0,2))}/terraform.tfstate"
   env_region = "${var.remote_region}"
 
   az_count = "${var.az_count}"
