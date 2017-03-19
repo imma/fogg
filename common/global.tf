d86793c7b457de639f97cf5a98944351dd5fd457
Defn Nghiem <iam@defn.sh>
2017-03-19 07:02:38 -0700

    remove aws_region, ue remote_Region

--- common/global.tf
+++ common/global.tf
@@ -5,10 +5,5 @@ variable "remote_region" {}
 module "global" {
   source = "../../module/global"
 
-  aws_region  = "${var.aws_region}"
   domain_name = "${var.domain_name}"
-}
-
-output org {
-  value = "${data.external.org.result}"
 }
--- module/global/variables.tf
+++ module/global/variables.tf
@@ -1,15 +1,9 @@
 variable "domain_name" {}
 
-variable "aws_region" {}
-
 output "aws_account_id" {
   value = "${data.aws_caller_identity.current.account_id}"
 }
 
 output "domain_name" {
   value = "${var.domain_name}"
-}
-
-output "aws_region" {
-  value = "${var.aws_region}"
 }
