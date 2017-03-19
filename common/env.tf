bcd193ffa4537d7dba05e704aa1ccfebcb3981a2
Defn Nghiem <iam@defn.sh>
2017-03-19 04:54:59 -0700

    split by underscore

--- common/env.tf
+++ common/env.tf
@@ -3,7 +3,7 @@ module "env" {
   source = "../../../module/env"
 
   global_bucket = "${var.remote_bucket}"
-  global_key    = "${slice(split("-",var.remote_path),0,1)}/terraform.tfstate"
+  global_key    = "${slice(split("_",var.remote_path),0,1)}/terraform.tfstate"
   global_region = "${var.remote_region}"
 
   az_count = "${var.az_count}"
--- common/service.tf
+++ common/service.tf
@@ -3,15 +3,15 @@ module "service" {
   source = "../../../../../module/service"
 
   global_bucket = "${var.remote_bucket}"
-  global_key    = "${slice(split("-",var.remote_path),0,1)}/terraform.tfstate"
+  global_key    = "${slice(split("_",var.remote_path),0,1)}/terraform.tfstate"
   global_region = "${var.remote_region}"
 
   env_bucket = "${var.remote_bucket}"
-  env_key    = "${slice(split("-",var.remote_path),0,2)}/terraform.tfstate"
+  env_key    = "${slice(split("_",var.remote_path),0,2)}/terraform.tfstate"
   env_region = "${var.remote_region}"
 
   app_bucket = "${var.remote_bucket}"
-  app_key    = "${slice(split("-",var.remote_path),0,3)}/terraform.tfstate"
+  app_key    = "${slice(split("_",var.remote_path),0,3)}/terraform.tfstate"
   app_region = "${var.remote_region}"
 
   az_count     = "${var.az_count}"
