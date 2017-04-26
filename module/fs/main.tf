a8782e46579aaec0c05ca8c28e8903680e464dde
Defn Nghiem <iam@defn.sh>
2017-04-25 21:49:07 -0700

    allow efs to egress

--- module/fs/main.tf
+++ module/fs/main.tf
@@ -37,6 +37,15 @@ resource "aws_security_group" "fs" {
   count = "${var.want_fs}"
 }
 
+resource "aws_security_group_rule" "fs_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.fs.id}"
+}
+
 resource "aws_efs_file_system" "fs" {
   tags {
     "Name"      = "${var.fs_name}"
