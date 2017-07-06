f8fa2040e6b4ef5da154fe43d38532d2bc4f751e
Defn Nghiem <iam@defn.sh>
2017-07-06 13:39:31 -0700

    depends on s3 logging bucket

--- module/env/main.tf
+++ module/env/main.tf
@@ -424,6 +424,8 @@ resource "aws_s3_bucket" "s3" {
 resource "aws_s3_bucket" "cloudfront" {
   bucket = "b-${format("%.8s",sha1(data.terraform_remote_state.global.aws_account_id))}-${var.env_name}-cloudfront"
 
+  depends_on = ["aws_s3_bucket.s3"]
+
   logging {
     target_bucket = "b-${format("%.8s",sha1(data.terraform_remote_state.global.aws_account_id))}-${var.env_name}-s3"
     target_prefix = "log/"
