1354e03dd77026feadb1e55944936bd3b13c8374
Defn Nghiem <iam@defn.sh>
2017-07-11 00:14:01 -0700

    turn peering-net-stag-prod into a top level fogg module

--- module/peering-net-stag-prod/main.tf
+++ module/peering-net-stag-prod/main.tf
@@ -1,15 +1,3 @@
-variable "remote_bucket" {}
-
-variable "remote_region" {}
-
-variable "remote_key_org" {}
-
-variable "remote_key_net" {}
-
-variable "remote_key_stag" {}
-
-variable "remote_key_prod" {}
-
 data "terraform_remote_state" "org" {
   backend = "s3"
 
---
+++ module/peering-net-stag-prod/variables.tf
@@ -0,0 +1,11 @@
+variable "remote_bucket" {}
+
+variable "remote_region" {}
+
+variable "remote_key_org" {}
+
+variable "remote_key_net" {}
+
+variable "remote_key_stag" {}
+
+variable "remote_key_prod" {}
