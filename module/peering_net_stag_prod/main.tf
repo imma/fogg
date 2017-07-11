f897a86443daef594313e66972382b3aae3eb1b6
Defn Nghiem <iam@defn.sh>
2017-07-11 00:30:22 -0700

    must have outputs

--- module/peering_net_stag_prod/main.tf
+++ module/peering_net_stag_prod/main.tf
@@ -71,27 +71,3 @@ module "peer_prod_stag" {
   this_vpc_id = "${data.terraform_remote_state.env_prod.vpc_id}"
   that_vpc_id = "${data.terraform_remote_state.env_stag.vpc_id}"
 }
-
-output "net_cidr_block" {
-  value = "${data.aws_vpc.net.cidr_block}"
-}
-
-output "stag_cidr_block" {
-  value = "${data.aws_vpc.stag.cidr_block}"
-}
-
-output "prod_cidr_block" {
-  value = "${data.aws_vpc.prod.cidr_block}"
-}
-
-output "prod_stag_peer_id" {
-  value = "${module.peer_prod_stag.peer_id}"
-}
-
-output "prod_net_peer_id" {
-  value = "${module.peer_prod_net.peer_id}"
-}
-
-output "stag_net_peer_id" {
-  value = "${module.peer_stag_net.peer_id}"
-}
