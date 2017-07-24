44800927c1ce72885e080a6560428c48670d0d8c
Defn Nghiem <iam@defn.sh>
2017-07-23 17:21:43 -0700

    add org to env

--- common/env.tf
+++ common/env.tf
@@ -5,3 +5,14 @@ module "env" {
   global_key    = "${join("_",slice(split("_",var.remote_path),0,1))}/terraform.tfstate"
   global_region = "${var.remote_region}"
 }
+
+data "terraform_remote_state" "org" {
+  backend = "s3"
+
+  config {
+    bucket     = "${var.remote_bucket}"
+    key        = "${join("_",slice(split("_",var.remote_path),0,1))}/terraform.tfstate"
+    region     = "${var.remote_region}"
+    lock_table = "terraform_state_lock"
+  }
+}
